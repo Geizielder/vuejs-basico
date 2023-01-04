e84d5db0920aec044fcc2cc6d24d6cffe23d7d77
¨Geizielder¨ <geizielder.gervazio@gmail.com>
2023-01-04 18:12:31 -0300

    Finalizando modulo basico com VueJs v.2 e v.3

--- vuejs-v3/projeto-vue-padrao/src/components/HelloWorld.vue
+++ vuejs-v3/projeto-vue-padrao/src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
 <template>
- <h1>Componente HelloWorld</h1>
+  <div>
+    <h1>Componente HelloWorld {{variavel}} </h1>
+    <p>Link da API {{url_api}} </p>
+    <p>Modo {{modo}} </p>
+    <p>Token {{token}} </p>
+  </div>
 </template>
 
 <script>
+
 export default {
+  data: () => ({
+    variavel: process.env.VUE_APP_MINHA_VARIALVE_DE_AMBIENTE,
+    url_api: process.env.VUE_APP_API_URL,
+    modo: process.env.VUE_APP_MODO,
+    token: process.env.VUE_APP_TOKEN
+  }),
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
 }
 </script>
 
